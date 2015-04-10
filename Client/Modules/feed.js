d73dfccfdc9a8827a5c10a5c38d94c5e63732724
Enja Reyes <enja.reyes@gmail.com>
2015-04-10 11:36:52 -0500

    hello world

--- Client/Modules/feed.js
+++ Client/Modules/feed.js
@@ -2,3 +2,7 @@
 // model 
 // view 
 // controller
+
+
+
+//show / hide public vs private feed with authentication
--- Client/Modules/profile.js
+++ Client/Modules/profile.js
@@ -1,3 +1,10 @@
 // model 
 // view 
 // controller
+
+
+
+//starred and saved devmeets
+//set up user home page / profile page
+//hello persons name
+//
