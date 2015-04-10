288b5a0b83d192fe25aa4ee21119afe12754767d
Sharky <kaydenalthen@gmail.com>
2015-04-10 17:29:42 -0500

    database up and running

--- Server/schema.sql
+++ Server/schema.sql
@@ -21,3 +21,35 @@
     -- location
     -- title
     -- description
+
+CREATE DATABASE DevMeet;
+
+Use DevMeet;
+
+CREATE TABLE users(
+        id SERIAL
+    ,   first_name VARCHAR(30)
+    ,   email VARCHAR(40)
+    ,   password VARCHAR(50)
+    ,   PRIMARY KEY (id)
+);
+
+CREATE TABLE meetups(
+        id SERIAL
+    ,   name VARCHAR(30)
+    ,   description VARCHAR(500)
+    ,   start_time TIMESTAMP
+    ,   end_time TIMESTAMP
+    ,   PRIMARY KEY (id)
+);
+
+
+CREATE TABLE starred(
+        id SERIAL
+    ,   meetup_id INT
+    ,   user_id INT 
+    ,   FOREIGN KEY (meetup_id) REFERENCES meetups(id)
+    ,   FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
