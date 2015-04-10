761a511ce5409f4f2b754c9f24f5411084733ab8
Enja Reyes <enja.reyes@gmail.com>
2015-04-09 19:45:12 -0500

    file outlines

---
+++ Server/schema.sql
@@ -0,0 +1,23 @@
+
+-- create database DevMeets
+-- use DevMeets
+
+  -- create table users
+    -- id  (PRIMARYKEY) (integer)
+    -- first name (varchar 50)
+    -- email (varchar 50)
+    -- password (varchar 50)
+
+
+  -- create table saved-starred 
+    -- user id (integer) FOREIGN KEY
+    -- meetup id (integer) FOREIGN KEY
+
+
+  -- create table austin meetups
+    -- id (PRIMARY KEY)
+    -- date 
+    -- time
+    -- location
+    -- title
+    -- description
