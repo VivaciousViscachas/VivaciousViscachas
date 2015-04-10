0869483b4336ac3e1856197d35bd4c7dbbfb733c
Enja Reyes <enja.reyes@gmail.com>
2015-04-10 16:07:37 -0500

    signup module / index.html changes

--- Client/Modules/profile.js
+++ Client/Modules/profile.js
@@ -2,7 +2,19 @@
 // view 
 // controller
 
+var profile = {}
 
+profile.model = function(){
+
+};
+
+profile.controller = function(){
+  var ctrl = this;
+};
+
+profile.view = function(){
+
+};
 
 //starred and saved devmeets
 //set up user home page / profile page
