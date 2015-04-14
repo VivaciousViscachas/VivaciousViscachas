1519773ee993e59a1db4db1eabed1816653f1cf0
Enja Reyes <enja.reyes@gmail.com>
2015-04-14 13:53:04 -0500

    fixed bug

--- Server/auth.js
+++ Server/auth.js
@@ -1,33 +1,12 @@
-var jwt = require('jwt-simple'), 
-    pg = require('pg');
-
 module.exports= {
   signup:function(request, response){
-    var firstName = request.body.firstName
-    var email = request.body.email
-    var password = request.body.password
-    var databaseUrl = process.env.DATABASE_URL || 'postgres://localhost/devmeet'
-    var userObj = {'firstName': firstName, 'email': email, 'password':password}
-
-    pg.connect(databaseUrl, function(err, client, done){
-      client.query('SELECT firstName FROM users WHERE email=' + email, function(err, result){ //check DB for email
-        done();
-        if(err){ //if doesn't exist in DB
-          //add user to DB
-          var token = jwt.encode(userObj, 'secret') //create the token
-          response.send({token: token}) //res.json or res.send?
-        }
-        else { 
-          console.log('user already exists')
-        };
-      })
-    })
+      var firstName = request.body.firstName
+      var email = request.body.email
+      var password = request.body.password
   },
   
   signin:function(request, response){
-    var email = request.body.email
-    var password = request.body.password
-    var databaseUrl = process.env.DATABASE_URL || 'postgres://localhost/devmeet'
-
+      var email = request.body.email
+      var password = request.body.password
   }
 }
