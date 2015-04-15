339ca96b34e7a41f8886ff307e5b2b59039a4ebb
Enja Reyes <enja.reyes@gmail.com>
2015-04-15 10:18:45 -0500

    fixed path errors

--- Server/auth.js
+++ Server/auth.js
@@ -1,5 +1,7 @@
 var jwt = require('jwt-simple'), 
-    pg = require('pg');
+    pg = require('pg'),
+    bcrypt = require('bcrypt-nodejs');
+
 
 module.exports= {
   signup:function(request, response){
@@ -8,16 +10,19 @@ module.exports= {
     var password = request.body.password
     var databaseUrl = process.env.DATABASE_URL || 'postgres://localhost/devmeet'
     var userObj = {'firstName': firstName, 'email': email, 'password':password}
+    var salt = bcrypt.genSaltSync(10);
 
     pg.connect(databaseUrl, function(err, client, done){
       client.query('SELECT firstName FROM users WHERE email=' + email, function(err, result){ //check DB for email
         done();
         if(err){ //if doesn't exist in DB
           //add user to DB
-          var token = jwt.encode(userObj, 'secret') //create the token
-          response.send({token: token}) //res.json or res.send?
-        }
-        else { 
+          var hash = bcrypt.hashSync(password, salt); 
+          client.query('INSERT INTO users (first_name, email, password) VALUES ($1, $2, $3)',[firstName, email, hash], function(){ 
+            var token = jwt.encode(userObj, 'secret') //create the token
+            response.send({token: token}) //res.json or res.send?
+          }
+        } else { 
           console.log('user already exists')
         };
       })
@@ -28,6 +33,24 @@ module.exports= {
     var email = request.body.email
     var password = request.body.password
     var databaseUrl = process.env.DATABASE_URL || 'postgres://localhost/devmeet'
+    var databasePassword;
 
+    pg.connect(databaseUrl, function(err, client, done){
+      client.query('')
+    }
+
+
+
+    bcrypt.compare(password, this.get('password'), function(err, same) {
+      cb(same)
+    });
   }
 }
+
+
+
+
+
+
+
+
