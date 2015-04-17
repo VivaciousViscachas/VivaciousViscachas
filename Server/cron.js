35f37b5f7a054ba4cfb7c40907f5abe5b4ed1e38
Gerald Tilma <gerald.tilma@gmail.com>
2015-04-17 14:22:04 -0500

    Working cron job (every 12am)

--- Server/cron.js
+++ Server/cron.js
@@ -1,12 +1,19 @@
-var request = require('request-promise');
-var databaseUrl = process.env.DATABASE_URL || 'postgres://localhost/devmeet';
+var CronJob = require('cron').CronJob,
+    request = require('request-promise'),
+    databaseUrl = process.env.DATABASE_URL || 'postgres://localhost/devmeet',
+    db = require('knex')({
+      client: 'pg',
+      connection: databaseUrl
+    });
 
-var db = require('knex')({
-  client: 'pg',
-  connection: databaseUrl
-});
+var update = new CronJob('00 00 00 * * *',
+  callAPI,
+  function(){ console.log('Cron called API') }, // function to run when callAPI is done
+  true, // start now
+  'America/Chicago'
+);
 
-module.exports.test = function () {
+function callAPI () {
   // Call the API to get the next tech meetups in Austin
   request('https://api.meetup.com/2/open_events?&category=34&status=upcoming&zip=78701&radius=20&key='+process.env.MEETUP_KEY)
     .then(function (response){
@@ -55,7 +62,6 @@ module.exports.test = function () {
 
           // if meetup, check last updated time, update if necessary
           } else if (Number(record.event_updated) !== meetup.updated) {
-
             db('meetups')
               .where('api_event_id', meetup.id)
               .update({
@@ -65,14 +71,15 @@ module.exports.test = function () {
                 event_time: meetup.time,
                 event_updated: meetup.updated,
                 event_duration: meetup.duration,
-                venue_address: meetup.venue.address_1,
-                venue_city: meetup.venue.city,
-                venue_state: meetup.venue.state,
-                venue_lat: meetup.venue.lat,
-                venue_lon: meetup.venue.lon,
-                group_name: meetup.group.name,
-                group_urlname: meetup.group.urlname,
-                group_how_to_find: meetup.how_to_find_us
+                venue_address: meetup.venue && meetup.venue.address_1,
+                venue_city: meetup.venue && meetup.venue.city,
+                venue_state: meetup.venue && meetup.venue.state,
+                venue_lat: meetup.venue && meetup.venue.lat,
+                venue_lon: meetup.venue && meetup.venue.lon,
+                group_name: meetup.group && meetup.group.name,
+                group_urlname: meetup.group && meetup.group.urlname,
+                group_how_to_find: meetup.group && meetup.how_to_find_us,
+                api_group_id: meetup.group && meetup.group.id
               })
               .catch(function(err){
                 console.log('ERROR ON UPDATE:', err);
