67cde4187dee0e69e6c0a41ddd36a344b46362b3
Samuel Thomas <samuelfullerthomas@gmail.com>
2015-04-20 11:47:34 -0500

    staring works, and inserts into db

--- Client/Modules/feed.js
+++ Client/Modules/feed.js
@@ -15,14 +15,16 @@ feed.controller = function () {
   }
   //gets the meetups from the DB and then stores them in localStorage
   mctrl.getMeetups = function () {
-   return m.request({method:"GET", url:"/meetups"}).then(function(result){
+   return m.request({method:"GET", url:"/feed"}).then(function(result){
     localStorage.setItem('meetups', JSON.stringify(result))
    })
   }
   mctrl.star = function (meetup) {
     console.log(meetup)
     mctrl.listOfMeetups[meetup.id-1].starred = true;
-    var test = {test: meetup};
+    var email = JSON.parse(localStorage.getItem('session')).email
+    console.log(email)
+    var test = {meetup: meetup, email:email};
     return m.request({method:"POST", url:"/star", data:test}).then(function(result){
       console.log(JSON.parse(JSON.stringify(result)))
     })
@@ -32,11 +34,11 @@ feed.controller = function () {
 }
 
 feed.view = function (ctrl) {
-  return m('div', [
+  return m('div.feed', [
     mctrl.listOfMeetups.map((function(meetup){
       return m('ul', [
         m('li', [
-          m('div', meetup.id), m('div', meetup.event_name), m('button', {class: 'unstarred'}, {onclick: function(){ console.log('hi')}})
+          m('div', meetup.id), m('div', meetup.event_name), m('button.starred', {onclick: function(){ mctrl.star(meetup)}})
         ])
       ])
     }))
