a5190e1b1d67623a1cb90db1487116ffbb4769ad
Enja Reyes <enja.reyes@gmail.com>
2015-04-20 10:45:33 -0500

    Added logout feature, added session.js, refactored routing on index.html

--- Client/Modules/profile.js
+++ Client/Modules/profile.js
@@ -6,6 +6,7 @@ starred.model = function(userObj){
 };
 
 starred.controller = function(){
+  if (Session.isLoggedIn === false) return m.route('/')
   ctrl = this;
   ctrl.meetupList; 
   var email = {email: JSON.parse(localStorage.getItem('session')).email};
