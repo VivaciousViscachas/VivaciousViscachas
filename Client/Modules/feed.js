3ec51a6a17025eb36cbe644eb5d45c6456f3c2c6
Samuel Thomas <samuelfullerthomas@gmail.com>
2015-04-20 09:41:41 -0500

    Showing a star

--- Client/Modules/feed.js
+++ Client/Modules/feed.js
@@ -2,37 +2,43 @@ var feed = {}
 
 
 feed.model = function (item) {
-	this.name = item.name;
-	this.members = item.members;
+  this.id = item.id;
+  this.event_name = item.event_name;
+  this.starred = false;
 };
-
-//the feed list based off the database query
-feed.list = function(obj){
-	var list = obj;
-	return list;
-}
 
 feed.controller = function () {
   mctrl = this;
-  mctrl.listOfItems = feed.list;
+  mctrl.listOfItems = function(obj){
+      var list = obj;
+      return list;
+  }
   //gets the meetups from the DB and then stores them in localStorage
   mctrl.getMeetups = function () {
-   return m.request({method:"GET", url:"/feed"}).then(function(result){
+   return m.request({method:"GET", url:"/meetups"}).then(function(result){
     localStorage.setItem('meetups', JSON.stringify(result))
    })
   }
+  mctrl.star = function (meetup) {
+    console.log(meetup)
+    mctrl.listOfMeetups[meetup.id-1].starred = true;
+    var test = {test: meetup};
+    return m.request({method:"POST", url:"/star", data:test}).then(function(result){
+      console.log(JSON.parse(JSON.stringify(result)))
+    })
+  }
   mctrl.getMeetups();
+  mctrl.listOfMeetups = mctrl.listOfItems(JSON.parse(localStorage.getItem('meetups')))
 }
 
 feed.view = function (ctrl) {
   return m('div', [
-    mctrl.listOfItems(JSON.parse(localStorage.getItem('meetups'))).map((function(meetup){
+    mctrl.listOfMeetups.map((function(meetup){
       return m('ul', [
         m('li', [
-          m('div', meetup.name), m('div', meetup.members)
+          m('div', meetup.id), m('div', meetup.event_name), m('div', {class: 'unstarred'}, {onclick: function(){ mctrl.star(meetup)}})
         ])
       ])
     }))
   ])
 }
-
